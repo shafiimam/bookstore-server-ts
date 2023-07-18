8c6736c98ceebe454650e420974c4e1b2834af11
Shafi Imam <shafi.imam@springrain.io>
2023-07-18 16:00:00 +0600

    add book controller

---
+++ src/app/modules/book/book.controller.ts
@@ -0,0 +1,52 @@
+import { Request, Response } from 'express';
+import { IBook } from './book.interface';
+import catchAsync from '../../../shared/catchAsync';
+import bookService from './book.service';
+import sendResponse from '../../../shared/sendResponse';
+import pick from '../../../shared/pick';
+import { bookFilterableFields, bookSearchableFields } from './book.constant';
+
+const getAllBooks = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, bookFilterableFields);
+  console.log(
+    '🚀 ~ file: book.controller.ts:11 ~ getAllBooks ~ filters:',
+    filters,
+  );
+  const allBooks = await bookService.getAllBooks(filters);
+  sendResponse<IBook[]>(res, {
+    statusCode: 200,
+    success: true,
+    data: allBooks.data,
+    message: 'books fetched successfully',
+  });
+});
+
+const createBook = catchAsync(async (req: Request, res: Response) => {
+  const book = req.body;
+  const createdBook = await bookService.createNewBook(book);
+  sendResponse<IBook>(res, {
+    statusCode: 201,
+    success: true,
+    data: createdBook.data,
+    message: 'book created successfully',
+  });
+});
+
+const updateBook = catchAsync(async (req: Request, res: Response) => {
+  console.log('🚀 ~ file: book.controller.ts:updateBook ~ req:', req.body);
+  const { id } = req.params;
+  const book = req.body;
+  const updatedBook = await bookService.updateBook(id, book);
+  sendResponse<IBook>(res, {
+    statusCode: 200,
+    success: true,
+    data: updatedBook,
+    message: 'book updated successfully',
+  });
+});
+
+export default {
+  getAllBooks,
+  createBook,
+  updateBook,
+};
